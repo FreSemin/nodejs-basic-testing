0944c06508fafc3c035dfb7f918d89117cf72e61
fresemin <fresemin@gmail.com>
2024-02-11 14:32:42 +0300

    test: 4 create account

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,9 +1,12 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { BankAccount, getBankAccount } from '.';
+
+const initialBalance = 12;
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    const localBankAccount: BankAccount = getBankAccount(initialBalance);
+
+    expect(localBankAccount.getBalance()).toEqual(initialBalance);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
