33157138d5dc385e9e8dc4569d5e2daf7b6da392
fresemin <fresemin@gmail.com>
2024-02-11 13:53:04 +0300

    test: 3 resolve value

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,9 +1,14 @@
-// Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  // throwError,
+  // throwCustomError,
+  resolveValue,
+  // MyAwesomeError,
+  // rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    await expect(resolveValue(123)).resolves.toBe(123);
   });
 });
 
